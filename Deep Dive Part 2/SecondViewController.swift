8c893d4409b0f17b84047de185e7989b9a166df0
Nick Liu <hybrida666@icloud.com>
2023-05-30 21:40:10 +0800

    Second Viewcontroller

--- Deep Dive Part 2/SecondViewController.swift
+++ Deep Dive Part 2/SecondViewController.swift
@@ -6,9 +6,45 @@
 //
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
     
+    private var textField = UITextField()
+    private var button = UIButton()
+    var updateText: ((String) -> Void)?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        [textField, button].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        [textField, button].forEach { view.addSubview($0) }
+        
+        setupUI()
+        textField.delegate = self
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    func setupUI() {
+        button.setTitle("Button", for: .normal)
+        button.backgroundColor = .black
+        
+        textField.borderStyle = .roundedRect
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            textField.widthAnchor.constraint(equalToConstant: view.frame.width * 2/3),
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.heightAnchor.constraint(equalToConstant: 40),
+            
+            button.widthAnchor.constraint(equalToConstant: view.frame.width * 2/3),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.heightAnchor.constraint(equalToConstant: 40),
+            button.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 20)
+        ])
+    }
+    
+    @objc func buttonTapped() {
+        updateText?(textField.text ?? "")
+        navigationController?.popViewController(animated: true)
+    }
     
     
     
