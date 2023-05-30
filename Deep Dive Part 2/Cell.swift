e8c61acb39d3037451808c38c85feb1f8cedf4b1
Nick Liu <hybrida666@icloud.com>
2023-05-30 16:35:45 +0800

    first commit

---
+++ Deep Dive Part 2/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Deep Dive Part 2
+//
+//  Created by Nick Liu on 2023/5/30.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    static let reuseIdentifier = "\(Cell.self)"
+    
+}
